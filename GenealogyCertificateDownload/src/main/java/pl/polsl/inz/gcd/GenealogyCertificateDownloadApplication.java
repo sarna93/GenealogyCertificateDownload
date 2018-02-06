86eed054b4ae4da08bea9df5cc8c242b7031907b
Krzysztof Sarnicki <sarna93zsth@gmail.com>
2018-02-06 21:17:02 +0100

    Enabled Spring MVC - example with jsp and bootstrap

--- GenealogyCertificateDownload/src/main/java/pl/polsl/inz/gcd/GenealogyCertificateDownloadApplication.java
+++ GenealogyCertificateDownload/src/main/java/pl/polsl/inz/gcd/GenealogyCertificateDownloadApplication.java
@@ -2,10 +2,17 @@ package pl.polsl.inz.gcd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GenealogyCertificateDownloadApplication {
+public class GenealogyCertificateDownloadApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GenealogyCertificateDownloadApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GenealogyCertificateDownloadApplication.class, args);
 	}
