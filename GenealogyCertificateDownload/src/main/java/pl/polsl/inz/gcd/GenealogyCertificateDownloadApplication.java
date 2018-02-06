4b8b06c24f920a588cb579fbb18e72503b2cbea3
Krzysztof Sarnicki <sarna93zsth@gmail.com>
2018-02-06 03:22:36 +0100

    Created Spring Boot application

---
+++ GenealogyCertificateDownload/src/main/java/pl/polsl/inz/gcd/GenealogyCertificateDownloadApplication.java
@@ -0,0 +1,12 @@
+package pl.polsl.inz.gcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenealogyCertificateDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenealogyCertificateDownloadApplication.class, args);
+	}
+}
